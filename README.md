0a99bdb68883a990bcd99c8aee0f66092951f480
Daigo Fujiwara <daigo@daigofujiwara.com>
2023-03-11 16:16:21 -0500

    SUbmission time

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # 2023-baseballhackday-submission
 Project for 2023 Baseball Hack Day 
+
+Masataka Yoshida is coming to the Red Sox. How 
+
+
+## How does he compare to rest of Japanese hitters?
+
+OPS?
+
+Ichiro (2001)
+Tsuyoshi Shinjo (2001)
+So Taguchi (2002)
+Hideki Matsui (2003)
+Kazuo Matsui (2004)
+Tadahito Iguchi (2005)
+Kenji Johjima (2006)
+Akinori Iwamura (2007)
+Kosuke Fukudome (2008)
+Tsuyoshi Nishioka (2011)
+Norichika Aoki (2012)
+Munenori Kawasaki (2012)
+Shohei Ohtani (2018)
+Shogo Akiyama (2020)
+Yoshitomo Tsutsugo (2020)
+Seiya Suzuki (2022)
+Masataka Yoshida (2023)
+
+
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=suzuki001ich", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=shinjo001tsu", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=taguch001so-", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=matsui001hid", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=matsui001kaz", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=iguchi001tad", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=johjim001ken", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=iwamur001aki", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=fukudo001kos", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=nishio002tsu", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=aoki--001nor", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=kawasa001mun", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=otani-000sho", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=tsutsu000yos", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=akiyam000sho", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=suzuki001sei", "table", 1,"en_US")
+=IMPORTHTML("https://www.baseball-reference.com/register/player.fcgi?id=yoshid002mas", "table", 1,"en_US")
--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,182 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+import ichirodata from "./assets/data/ichiro.json";
+import kmatsuidata from "./assets/data/kmatsui.json";
+import shinjodata from "./assets/data/shinjo.json";
+import taguchidata from "./assets/data/taguchi.json";
+import hmatsuidata from "./assets/data/hmatsui.json"
+import iguchidata from "./assets/data/iguchi.json";
+import ohtanidata from "./assets/data/ohtani.json";
+import fukudomedata from "./assets/data/fukudome.json";
+import johjimadata from "./assets/data/johjima.json";
+import iwamuradata from "./assets/data/iwamura.json";
+import akiyamadata from "./assets/data/akiyama.json";
+import tsutsugodata from "./assets/data/tsutsugo.json";
+import suzukidata from "./assets/data/suzuki.json";
+import kawasakidata from "./assets/data/kawasaki.json";
+import aokidata from "./assets/data/aoki.json";
+import nishiokadata from "./assets/data/nishioka.json";
+
+import yoshidadata from "./assets/data/yoshida.json";
+
+// import reactLogo from './assets/react.svg'
 import './App.css'
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [datasource, setDatasource] = useState('ichiro')
+
+  let data = [];
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: 'OPS compared NPB/MLB',
+      },
+    },
+    responsive: true,
+    scales: {
+      x: {
+        stacked: true,
+      },
+      y: {
+        stacked: true,
+      },
+    },
+  };
+
+  const labels = ['Age 18', 'Age 19', 'Age 20', 'Age 21', 'Age 22', 'Age 23', 'Age 24', 'Age 25', 'Age 26', 'Age 27', 'Age 28', 'Age 29', 'Age 30', 'Age 31', 'Age 32', 'Age 33', 'Age 34', 'Age 35', 'Age 36', 'Age 37', 'Age 38', 'Age 39', 'Age 40', 'Age 41', 'Age 42', 'Age 43', 'Age 44', 'Age 45'];
+
+ // colors NPB: light blue #deebf7 dark blue #9ecae1
+ // MLB light red #fee6ce dark red #fdae6b
+
+  switch(datasource) {
+    case 'ohtani':
+      data = ohtanidata;
+      break;
+    case 'shinjo':
+      data = shinjodata;
+      break;
+    case 'hmatsui':
+      data = hmatsuidata;
+      break;
+    case 'kmatsui':
+      data = kmatsuidata;
+      break;
+    case 'iguchi':
+      data = iguchidata;
+      break;      
+    case 'taguchi':
+      data = taguchidata;
+      break;
+    case 'fukudome':
+      data = fukudomedata;
+      break;
+    case 'johjima':
+      data = johjimadata;
+      break;
+    case 'iwamura':
+      data = iwamuradata;
+      break;
+    case 'tsutsugo':
+      data = tsutsugodata;
+      break;
+    case 'aoki':
+      data = aokidata;
+      break;
+    case 'suzuki':
+      data = suzukidata;
+      break;
+    case 'akiyama':
+      data = akiyamadata;
+      break;
+    case 'kawasaki':
+      data = kawasakidata;
+      break;
+    case 'nishioka':
+      data = nishiokadata;
+      break;
+    case 'yoshida':
+      data = yoshidadata;
+      break;
+    default:
+      data = ichirodata;
+  };
+
+  console.log(datasource, data);
+
+  const chartdata = {
+    labels,
+    datasets: [
+      {
+        label: 'NPB OBP',
+        data: data['NOBP'],
+        backgroundColor: '#deebf7',
+      },
+      {
+        label: 'NPB SLG',
+        data: data['NSLUG'],
+        backgroundColor: '#9ecae1',
+      },
+      {
+        label: 'MLB OBP',
+        data: data['MOBP'],
+        backgroundColor: '#fee6ce',
+      },
+      {
+        label: 'MLB SLG',
+        data: data['MSLG'],
+        backgroundColor: '#fdae6b',
+      },
+    ],
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Comparing Japanese MLB hitters' OPS from Japanese league OPS</h1>
+      <p></p>
+      <select onChange={(e) => setDatasource(e.target.value)}>
+        <option value='ichiro'>Ichiro (2001)</option>
+        <option value='shinjo'>Tsuyoshi Shinjo (2001)</option>
+        <option value='taguchi'>So Taguchi (2002)</option>
+        <option value='hmatsui'>Hideki Matsui (2003)</option>
+        <option value='kmatsui'>Kazuo Matsui (2004)</option>
+        <option value='iguchi'>Tadahito Iguchi (2005)</option>
+        <option value='johjima'>Kenji Johjima (2006)</option>
+        <option value='iwamura'>Akinori Iwamura (2007)</option>
+        <option value='fukudome'>Kosuke Fukudome (2008)</option>
+        <option value='nishioka'>Tsuyoshi Nishioka (2011)</option>
+        <option value='aoki'>Norichika Aoki (2012)</option>
+        <option value='kawasaki'>Munenori Kawasaki (2012)</option>
+        <option value='ohtani'>Shohei Ohtani (2018)</option>
+        <option value='akiyama'>Shogo Akiyama (2020)</option>
+        <option value='tsutsugo'>Yoshitomo Tsutsugo (2020)</option>
+        <option value='suzuki'>Seiya Suzuki (2022)</option>
+        <option value='yoshida'>Masataka Yoshida (2023)</option>
+      </select>
+
+      <Bar options={options} data={chartdata} />
     </div>
   )
 }
